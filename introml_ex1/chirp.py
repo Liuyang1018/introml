ccbb9a4a06a859ada40f8d5194d7ec493c5f6d53
Emma An <787434822@qq.com>
2022-05-10 23:52:38 +0800

    Ex1 Sampling Theorem FINISH

--- introml_ex1/chirp.py
+++ introml_ex1/chirp.py
@@ -1,6 +1,17 @@
+import numpy as np
 
 
 def createChirpSignal(samplingrate: int, duration: int, freqfrom: int, freqto: int, linear: bool):
     # returns the chirp signal as list or 1D-array
     # TODO
-    return 0
+    t = np.linspace(0, duration, samplingrate)  # time scaled
+    if linear:
+        c = (freqto - freqfrom)/duration  # chirp rate
+        phase = 2 * np.pi * (freqfrom * t + 0.5 * c * t * t)
+        print("linear phase", type(phase))
+    else:
+        k = (freqto / freqfrom) ** (1 / duration)  # the rate of exponential change in frequency
+        phase = 2 * np.pi * freqfrom * ((k ** t - 1) / np.log(k))
+        print("exponential phase", type(phase))
+    return np.sin(phase)
+
--- introml_ex1/main.py
+++ introml_ex1/main.py
@@ -1,5 +1,18 @@
+import numpy as np
+
 from chirp import createChirpSignal
 from decomposition import createTriangleSignal, createSquareSignal, createSawtoothSignal
 
+import matplotlib.pyplot as plt
 
 # TODO: Test the functions imported in lines 1 and 2 of this file.
+chirp_linear = createChirpSignal(200, 1, 1, 10, True)
+chirp_exponential = createChirpSignal(200, 1, 1, 10, False)
+
+if __name__ == '__main__':
+
+    plt.plot(np.linspace(0, 1, 200), chirp_linear)
+    plt.show()
+
+    plt.plot(np.linspace(0, 1, 200), chirp_exponential)
+    plt.show()
