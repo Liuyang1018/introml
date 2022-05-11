ecbf753d9e3fa9e5aa6701f622c82fdd45273b67
Emma An <787434822@qq.com>
2022-05-11 19:46:56 +0800

    Ex1 Fourier Decomposition FINISH

--- introml_ex1/decomposition.py
+++ introml_ex1/decomposition.py
@@ -1,18 +1,31 @@
+import numpy as np
 
 
 def createTriangleSignal(samples: int, frequency: int, k_max: int):
     # returns the signal as 1D-array (np.ndarray)
     # TODO
-    return
+    t = np.linspace(0, 1, samples).reshape((samples, 1))
+    k = np.arange(k_max)
+    to_sum_up = (-1) ** k * ((np.sin(2 * np.pi * (2 * k + 1) * frequency * t)) / ((2 * k + 1) ** 2))
+    result = 8 / (np.pi ** 2) * np.sum(to_sum_up, axis=1)
+    return result
 
 
 def createSquareSignal(samples: int, frequency: int, k_max: int):
     # returns the signal as 1D-array (np.ndarray)
     # TODO
-    return
+    t = np.linspace(0, 1, samples).reshape((samples, 1))
+    k = np.arange(1, k_max)
+    to_sum_up = (np.sin(2 * np.pi * (2 * k - 1) * frequency * t)) / (2 * k - 1)
+    result = 4 / np.pi * np.sum(to_sum_up, axis=1)
+    return result
 
 
 def createSawtoothSignal(samples: int, frequency: int, k_max: int, amplitude: int):
     # returns the signal as 1D-array (np.ndarray)
     # TODO
-    return
+    t = np.linspace(0, 1, samples).reshape((samples, 1))
+    k = np.arange(1, k_max)
+    to_sum_up = (np.sin(2 * np.pi * k * frequency * t)) / k
+    result = amplitude / 2 - amplitude / np.pi * np.sum(to_sum_up, axis=1)
+    return result
--- introml_ex1/main.py
+++ introml_ex1/main.py
@@ -1,18 +1,33 @@
-import numpy as np
-
 from chirp import createChirpSignal
 from decomposition import createTriangleSignal, createSquareSignal, createSawtoothSignal
 
+import numpy as np
 import matplotlib.pyplot as plt
 
 # TODO: Test the functions imported in lines 1 and 2 of this file.
+
 chirp_linear = createChirpSignal(200, 1, 1, 10, True)
 chirp_exponential = createChirpSignal(200, 1, 1, 10, False)
 
+decomposition_triangle = createTriangleSignal(200, 2, 10000)
+decomposition_square = createSquareSignal(200, 2, 10000)
+decomposition_sawtooth = createSawtoothSignal(200, 2, 10000, 1)
+
 if __name__ == '__main__':
 
-    plt.plot(np.linspace(0, 1, 200), chirp_linear)
+    t = np.linspace(0, 1, 200)
+
+    plt.plot(t, chirp_linear)
     plt.show()
 
-    plt.plot(np.linspace(0, 1, 200), chirp_exponential)
+    plt.plot(t, chirp_exponential)
+    plt.show()
+
+    plt.plot(t, decomposition_triangle)
+    plt.show()
+
+    plt.plot(t, decomposition_square)
+    plt.show()
+
+    plt.plot(t, decomposition_sawtooth)
     plt.show()
